af81f9cd9267e8e09ceb1e99967ac0b69142bb27
Maksim Beliaev <beliaev.m.s@gmail.com>
2019-12-25 23:23:56 +0200

    1) added action on buttons for manual/auto settings

--- web_interface/js/control.js
+++ web_interface/js/control.js
@@ -4,44 +4,53 @@ var sunrise, sunset;
 
 // functions which are invoked on button click
 $('#submit').click(function(e){
-e.preventDefault();
+    e.preventDefault();
 
-min_temp = $('#min_temp').val();
-max_temp = $('#max_temp').val();
-min_humidity = $('#min_humidity').val();
-max_humidity = $('#max_humidity').val(); 
-sunrise = $('#sunrise').val();
-sunset = $('#sunset').val();
+    min_temp = $('#min_temp').val();
+    max_temp = $('#max_temp').val();
+    min_humidity = $('#min_humidity').val();
+    max_humidity = $('#max_humidity').val(); 
+    sunrise = $('#sunrise').val();
+    sunset = $('#sunset').val();
 
-if (min_temp == "" || min_temp > max_temp || min_temp < 0) {
-    return alert("Minimum temperature cannot be empty, negative or greater than Maximum temperature");
-}
-if (max_temp == "") {
-    return alert("Maximum temperature cannot be empty");
-}
-if (min_humidity == "" || min_humidity > max_humidity || min_humidity < 0) {
-    return alert("Minimum humidity cannot be empty, negative or greater than Maximum humidity");
-}
-if (max_humidity == "") {
-    return alert("Maximum humidity cannot be empty");
-}
-if (sunrise == "" || sunrise < 0) {
-    return alert("Sunrise cannot be empty or negative");
-}
-if (sunset == "" ||sunset < 0) {
-    return alert("Sunset cannot be empty or negative");
-}
-// use post request instead of get to send data only once
-// see security advantages of POST method
-$.post('/save?min_temp=' + min_temp + 
-      '&max_temp=' + max_temp + 
-      '&min_humidity=' + min_humidity + 
-      '&max_humidity=' + max_humidity + 
-      '&sunrise=' + sunrise + 
-      '&sunset=' + sunset, function(data){
-                                console.log(data);
-                            });
+    if (min_temp == "" || min_temp > max_temp || min_temp < 0) {
+        return alert("Minimum temperature cannot be empty, negative or greater than Maximum temperature");
+    }
+    if (max_temp == "") {
+        return alert("Maximum temperature cannot be empty");
+    }
+    if (min_humidity == "" || min_humidity > max_humidity || min_humidity < 0) {
+        return alert("Minimum humidity cannot be empty, negative or greater than Maximum humidity");
+    }
+    if (max_humidity == "") {
+        return alert("Maximum humidity cannot be empty");
+    }
+    if (sunrise == "" || sunrise < 0) {
+        return alert("Sunrise cannot be empty or negative");
+    }
+    if (sunset == "" ||sunset < 0) {
+        return alert("Sunset cannot be empty or negative");
+    }
+    // use post request instead of get to send data only once
+    // see security advantages of POST method
+    $.post('/save?min_temp=' + min_temp + 
+        '&max_temp=' + max_temp + 
+        '&min_humidity=' + min_humidity + 
+        '&max_humidity=' + max_humidity + 
+        '&sunrise=' + sunrise + 
+        '&sunset=' + sunset, function(data){
+                                    console.log(data);
+                                });
 });      
+
+// called when auto/manual settings are switched
+$('input[name="fan_options"]').change(function(e){
+    console.log(e.currentTarget.attributes.id.nodeValue);
+});  
+
+$('input[name="light_options"]').change(function(e){
+    console.log(e.currentTarget.attributes.id.nodeValue);
+});  
 
 // called to sync time
 $('#sync_time').click(function(e){
